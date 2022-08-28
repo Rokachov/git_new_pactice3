5c6f2fce0d41e21b9c86977cf07c1d38ba2c822a
jonatahrokachov <jonatah.rokachov29@gmail.com>
2022-08-28 16:38:10 -0400

    adding the siret word

--- src/sunday/NextTry.java
+++ src/sunday/NextTry.java
@@ -2,6 +2,9 @@ package sunday;
 
 public class NextTry {
     public static void main(String[] args) {
+
         System.out.println("Hello");
+
+        System.out.println("Wooden Spun");
     }
 }
